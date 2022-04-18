284dbcb1d59311caaff045e2b34017f4a424dd54
SharonMutabonwa <mutaron37@gmail.com>
2022-04-18 14:38:26 +0000

    Producing output according to a format

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
@@ -14,7 +15,16 @@ struct convert
 };
 typedef struct convert con_vert;
 
+
+int _putchar(char);
+int parser(const char *format, con_vert f_list[], va_list arg_list);
 int _printf(const char *format, ...);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+
 
 
 
