ad9cb4f4dcfed4a672ac9a8eae0099e28fc3a963
SharonMutabonwa <mutaron37@gmail.com>
2022-04-19 17:44:24 +0000

    Producing output according to a format

--- main.h
+++ main.h
@@ -3,6 +3,20 @@
 
 #include <stdarg.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct fmt - structure
+ * @F: char holding the format.
+ * @T: pointer function.
+ */
+typedef struct fmt
+{
+	char F;
+	int (*T)(va_list);
+} ft;
 
 
 int _putchar(char c);
