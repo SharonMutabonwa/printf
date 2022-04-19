ffc11da2d733f1b5746aa84d1783702e0c467f34
SharonMutabonwa <mutaron37@gmail.com>
2022-04-19 12:02:35 +0000

    Producing output according tp a format

--- _printf.c
+++ _printf.c
@@ -1,72 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _printfunc - get the right function corresponding to format specified
- * @fi: input format
- * Return:pointer to function that corresponds with specified format
- *
- */
-int (*_printfunc(char fi))(va_list)
-{
-	int i = 0;
-	print_f f[] = {
-		{'c', printchar},
-		{'s', printstr},
-		{'d', printint},
-		{'i', printint},
-		{'\0', NULL}
-	};
-	while (f[i].type)
-	{
-		if (fi == f[i].type)
-		{
-			return (f[i].funct);
-		}
-		i++;
-
-	}
-	return (NULL);
-}
-
-/**
- * _printf - function that produces output according to format passed
- * @format: character string containing 0 or more directive
- * Return: number of chars printed exluding null byte
+ * _printf - Function to work like a printf function
+ * @format: the format of printing character
+ * Return: result.
  */
 int _printf(const char *format, ...)
 {
-	int i, counter;
+	va_list valist;
+	int i = 0;
+	int result = 0;
 
-	int (*fn)(va_list);
-
-	va_list a_list;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(a_list, format);
-	i = counter = 0;
-
-	while (format[i] != '\0')
+	va_start(valist, format);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] == '\0')
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			fn = _printfunc(format[i + 1]);
-			if (fn == NULL)
-				counter += printNaN(format[i], format[i + 1]);
-			else
-				counter += fn(a_list);
+			break;
+		}
+		else if (format[i] == '%' && format[i + 1])
+		{
+			result += (*format_conversion(format[i + 1]))(valist);
 			i++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			counter++;
+			result += _putchar(format[i]);
 		}
-		i++;
 	}
-	va_end(a_list);
-	return (counter);
+	va_end(valist);
+	return (result);
 }
